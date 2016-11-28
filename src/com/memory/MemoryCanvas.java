9a2f9307bb68e10f30d53127751c7e9ad477b584
danielHPeters <daniel.peters.ch@gmail.com>
2016-11-28 19:12:47 +0100

    Initial commit

---
+++ src/com/memory/MemoryCanvas.java
@@ -0,0 +1,14 @@
+package com.memory;
+
+import com.memory.gameobjects.Card;
+
+import javax.swing.*;
+
+/**
+ * Created by Daniel on 28.11.2016.
+ */
+public class MemoryCanvas extends JPanel {
+
+    private Card[][] cards;
+
+}
---
+++ src/com/memory/gameobjects/Card.java
@@ -0,0 +1,21 @@
+package com.memory.gameobjects;
+
+import java.awt.*;
+
+/**
+ * Created by Daniel on 28.11.2016.
+ */
+public class Card {
+
+    private String name;
+    private Image image;
+
+    /**
+     *
+     */
+    public Card(String name, Image image){
+        this.name = name;
+        this.image = image;
+    }
+
+}
